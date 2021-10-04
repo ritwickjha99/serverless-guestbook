eb92b2a6ff8b4a8d896fc564bd47eed38a348d79
ritwickjha99 <99ritwickjha@gmail.com>
2021-10-04 23:29:02 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://815f5e18.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
